06720687bb178aa4209063ebf69bc07a6853a07f
Jeon Gyedong <jeongyedong@gmail.com>
2022-02-27 09:43:26 +0900

    ENH: Add API PATCH /users/me

--- app/api/api_v1/endpoints/users.py
+++ app/api/api_v1/endpoints/users.py
@@ -1,14 +1,37 @@
 from typing import Any
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body, HTTPException
+from fastapi.encoders import jsonable_encoder
+from pydantic.networks import EmailStr
 
 from app.api.depends import get_current_active_user
 from app.models.user import User
-from app.schemas.user import UserRead
+from app.schemas.user import UserRead, UserUpdate
+from app.api.depends import get_db
+from sqlalchemy.orm import Session
+from app.crud.user import crud_user
 
 router = APIRouter()
 
 
-@router.get("/me", response_model=UserRead, response_model_exclude={'password'})
+@router.get("/me", response_model=UserRead, response_model_exclude={"password"})
 def read_user_me(current_user: User = Depends(get_current_active_user)) -> UserRead:
     return current_user
+
+
+@router.patch('/me', response_model=UserRead, response_model_exclude={"password"})
+def update_user_me(*, db: Session = Depends(get_db), password: str = Body(None), email: EmailStr = Body(None),current_user: User = Depends(get_current_active_user)):
+    current_user_data = jsonable_encoder(current_user)
+    user_in = UserUpdate(**current_user_data)
+    if password:
+        user_in.password = password
+    else:
+        del user_in.password
+    if email:
+        user_has_same_email = crud_user.get_by_email(db=db, email=email)
+        if user_has_same_email:
+            raise HTTPException(status_code=400, detail="Email already registered")
+        else:
+            user_in.email = email
+    user = crud_user.update(db, db_obj=current_user, obj_in=user_in)
+    return user
--- app/crud/user.py
+++ app/crud/user.py
@@ -10,7 +10,6 @@ from app.schemas.user import UserCreate, UserUpdate
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
-        print(f'get_by_email email: {email}')
         return db.query(User).filter(User.email == email).first()
 
     def create(self, db: Session, *, obj_in: UserCreate) -> User:
@@ -23,17 +22,14 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         db.refresh(db_obj)
         return db_obj
 
-    def update(
-        self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
-    ) -> User:
+    def update(self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]) -> User:
         if isinstance(obj_in, dict):
             update_data = obj_in
         else:
             update_data = obj_in.dict(exclude_unset=True)
-        if update_data["password"]:
+        if "password" in update_data:
             hashed_password = get_password_hash(update_data["password"])
-            del update_data["password"]
-            update_data["hashed_password"] = hashed_password
+            update_data["password"] = hashed_password
         return super().update(db, db_obj=db_obj, obj_in=update_data)
 
     def authenticate(self, db: Session, *, email: str, password: str) -> Optional[User]:
